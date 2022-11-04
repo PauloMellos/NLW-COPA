70f379927db91e6f78e3935b9e41e71b1fac3cbc
PAULO MELLOS <101419982+PauloMellos@users.noreply.github.com>
2022-11-04 15:54:50 -0300

    brazil x argentina

--- main.js
+++ main.js
@@ -29,4 +29,4 @@ document.querySelector("#cards").innerHTML =
     createGame("switzerland", "13:00", "brazil") +
       createGame("portugal", "16:00", "uruguai")
   ) +
-  createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))
+  createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon")) + createCard("02/12", "sexta", createGame("brazil", "13:00", "Argentina"))
